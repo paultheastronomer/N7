fcc9512152ab9ebed89d16692116a0b00ba7504d
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-08-08 15:46:15 +0200

    updates

--- plots/exocomets.py
+++ plots/exocomets.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
+from scipy.interpolate import interp1d
 
 def Initialise():
     with open('../params.json') as param_file:    
@@ -15,8 +16,6 @@ def Bin_data(x,y1,e1,bin_pnts):
     bin_y       = np.array([y1[digitized == i].mean() for i in range(0, len(bins))])
     bin_e       = np.array([e1[digitized == i].mean() for i in range(0, len(bins))])
     return bins, bin_y, bin_e/np.sqrt(bin_pnts)
-
- 
     
 def main():    
     # Read all parameters from params.json file.
@@ -25,15 +24,30 @@ def main():
     # Define the data directory
     dat_directory   = param["directories"]["workdir"]
 
-    wc, fc, ec      = np.genfromtxt(dat_directory+'N_no2014_2016_07_27.txt',unpack=True)    
+    # Load 2014 data
+    W, RV, f0_0, e0_0, f_AG_0, e_AG_0   = np.genfromtxt(dat_directory+'B_N_2014.dat',unpack=True)
     
-    w1, f1, e1      = np.genfromtxt(dat_directory+'N_2015v1_2016_07_27.txt',unpack=True)
-    w2, f2, e2      = np.genfromtxt(dat_directory+'N_2015v2_2016_07_27.txt',unpack=True)
-    w3, f3, e3      = np.genfromtxt(dat_directory+'N_2016v1_2016_07_27.txt',unpack=True)
+    # Load the data with little airglow
+    W1, F1, E1  = np.genfromtxt(dat_directory+'N_AG_not_subtracted_2016_08_08_10Dec.txt',unpack=True)
+    W2, F2, E2  = np.genfromtxt(dat_directory+'N_AG_not_subtracted_2016_08_08_26Dec.txt',unpack=True)
+    W3, F3, E3  = np.genfromtxt(dat_directory+'N_AG_not_subtracted_2016_08_08_30Jan.txt',unpack=True)
     
-    W, FitNoPS, F, E, C, Fit, N1, N2, N3      = np.genfromtxt(dat_directory+'NI_fit_2016_08_01.dat',unpack=True,skip_header=3)
+    # Load all combined data (except 2014 data)
+    Wc, Fc, Ec  = np.genfromtxt(dat_directory+'N_AG_not_subtracted_2016_08_08.txt',unpack=True)
     
+    # Load stellar model convolved with beta Pic broadening and shifted by +20.5 km/s (RV of Beta Pic).
+    Ws, Fs      = np.genfromtxt('/home/paw/science/betapic/data/stellar/BP_Phoenix_4.5_Conv.dat',unpack=True)
     
+    # Calculate the factor between the stellar model with data
+    factor = np.median(Fc[7720:7850]/Fs[7720:7850])
+    
+    # Normalise the stellar model
+    Fs = Fs*factor
+    
+    # Find the normalisation number used to show the divided spectra next to measured data.
+    norm = np.median(Fc[7720:7850])
+    
+    # Configuring the plotting window
     #fig = plt.figure(figsize=(6.5,4.5))
     fig = plt.figure(figsize=(11,8))
     fontlabel_size  = 18
@@ -44,35 +58,40 @@ def main():
     plt.rcParams['text.usetex'] = True
     plt.rcParams['text.latex.unicode'] = True
     
-    bin_pnts = 3
+    bin_pnts    = 3
     
-    wcb, fcb, ecb   = Bin_data(wc,fc,ec,bin_pnts)
+    f2          = np.interp(Wc, Ws, Fs)
     
-    w1b, f1b, e1b   = Bin_data(w1,f1,e1,bin_pnts)
-    w2b, f2b, e2b   = Bin_data(w2,f2,e2,bin_pnts)
-    w3b, f3b, e3b   = Bin_data(w3,f3,e3,bin_pnts)
+    DIV         = norm*(Fc/f2)
     
-    Wb, Fb, Diffb    = Bin_data(W,F,F-Fit,bin_pnts)
+    # Binning the combined data
+    Wcb, Fcb, Ecb   = Bin_data(Wc,Fc,Ec,bin_pnts)
+    Wcb, DIVb, Ecb   = Bin_data(Wc,DIV,Ec,bin_pnts)
+    
+    # Uncomment text below to see each epoch of data.
+    '''
+    w1b, f1b, e1b   = Bin_data(W,F1,E1,bin_pnts)
+    w2b, f2b, e2b   = Bin_data(W,F2,E2,bin_pnts)
+    w3b, f3b, e3b   = Bin_data(W,F3,E3,bin_pnts)
+    plt.step(w1b, f1b, color="#FF9303", label=r'Method 1')
+    plt.step(w2b, f2b, color="#0386FF", label=r'Method 1')
+    plt.step(w3b, f3b, color="#00B233", label=r'Method 1')
+    '''
+    
+    #plt.step(Ws,Fs,lw=2., color="green")
+    plt.step(Wcb,Fcb,lw=2., color="black")
+    
 
     
-    #plt.plot(W,N1,lw=1,color="blue")
-    #plt.plot(W,N2,lw=1,color="green")
-    #plt.plot(W,Fit,lw=2,color="red")
+    plt.step(Wcb,DIVb,lw=2., color="red")
     
-    #plt.step(wc, fc, lw=2, color="black", label=r'Method 1')
-    #plt.step(W, F, lw=2, color="green", label=r'Method 1')
+    np.savetxt(dat_directory+"N_DIV.dat",np.column_stack((Wc, DIV, Ec)))
     
-    plt.plot(W,Fit,lw=2., color="red")
-    plt.step(Wb,Fb,lw=2., color="black")
+    #'''
+    #plt.step(Wc[7720:7850],Fc[7720:7850],lw=2., color="blue")
     
-    plt.plot(W,F-F,lw=2,color="gray")
-    plt.step(Wb,Diffb,lw=2., color="blue")
-    
-    #plt.step(w1b, f1b, label=r'Method 1')
-    #plt.step(w2b, f2b, label=r'Method 1')
-    #plt.step(w3b, f3b, label=r'Method 1')
     plt.xlim(1198.3,1202.3)
-    plt.ylim(-2.5e-14,2.2e-14)
+    plt.ylim(-1.0e-16,2.6e-14)
     plt.show()  
     
 if __name__ == '__main__':
--- src/convert2owens.py
+++ src/convert2owens.py
@@ -13,10 +13,10 @@ def main():
     # Define the data directory
     dat_directory   = param["directories"]["workdir"]
 
-    W, F, E         = np.genfromtxt(dat_directory+"N_no2014_2016_07_27.txt"
+    W, F, E         = np.genfromtxt(dat_directory+"N_DIV.dat"
                       ,unpack=True,skip_header=500,skip_footer= 500)
 
-    f = open(dat_directory+'NI_no2014_owens.dat', 'w+')
+    f = open(dat_directory+'N_DIV_owens.dat', 'w+')
     for i in range(len(W)):
         if not np.isnan(F[i]) and F[i] > 0:
             print >> f, " ","{: 1.10e}".format(W[i])," "+"{: 1.10e}".format(F[i])," "+"{: 1.10e}".format(E[i])
