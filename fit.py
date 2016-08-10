fd4a1fdc981c9aa4e7be7d52901a80a03b76cc72
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-08-10 15:49:34 +0200

    not usable. temp storage

--- fit.py
+++ fit.py
@@ -26,10 +26,10 @@ def FindBestParams(params,F,E,Const,ModelType, param):
     best_P, success = leastsq(s.chi2_lm, params, args=(F,E,Const,ModelType, param), maxfev=1000)
     return best_P
 
-def BasicPlot(param,W,F,E,l,f_fit,f_abs_ism,f_abs_bp):
+def BasicPlot(param,W,F,E,l,f_fit,f_abs_ism,f_abs_bp,f_abs_X):
 
-    c1  = param["fit"]["continuum"]["cut1"]
-    c2  = param["fit"]["continuum"]["cut2"]
+    c1  = param["fit"]["windows"]["window1"]["cut1"]
+    c2  = param["fit"]["windows"]["window1"]["cut2"]
 
     if param["display"]["bin"] > 1:
         bin_size = param["display"]["bin"]
@@ -46,16 +46,28 @@ def BasicPlot(param,W,F,E,l,f_fit,f_abs_ism,f_abs_bp):
     
     plt.plot(W,f_fit,lw=3,color='#FF281C',label=r'Best fit')
     plt.plot(l,f_abs_ism,color="green",lw=3)
-    plt.plot(l,f_abs_bp,color="blue",lw=3)    
-    plt.xlim(param["display"]["window"]["x1"],param["display"]["window"]["x2"])
-    plt.ylim(param["display"]["window"]["y1"],param["display"]["window"]["y2"])
+    plt.plot(l,f_abs_bp,color="blue",lw=3)   
+    plt.plot(l,f_abs_X,color="purple",lw=3)  
+    plt.xlim(param["display"]["window1"]["x1"],param["display"]["window1"]["x2"])
+    plt.ylim(param["display"]["window1"]["y1"],param["display"]["window1"]["y2"])
     plt.show()
 
 def PrintParams(P):
-    print "\tlog(N/1cm^2)\t=\t",P[0]
-    #print "\t   v\t\t=\t",P[1],"km/s"
-    #print "\t   b\t\t=\t",P[2],"km/s"
-    print "\n"
+    print "\nISM:"
+    print "-"*50
+    print "\t     b\t\t=\t",P[0],"km/s"
+
+    print "\nCS:"
+    print "-"*50
+    print "\tlog(N/1cm^2)\t=\t",P[1]
+    print "\t     b\t\t=\t",P[2],"km/s"
+
+    print "\nExocomet:"
+    print "-"*50
+    print "\tlog(N/1cm^2)\t=\t",P[3],"km/s"
+    print "\t    RV\t\t=\t",P[4],"km/s"
+    print "\t     b\t\t=\t",P[5],"km/s"
+    print "-"*50,"\n"
 
 def main():
     # Read all parameters from params.json file.
@@ -65,57 +77,79 @@ def main():
     dat_directory   = param["directories"]["workdir"]
 
     # Select the model type
-    ModelType       = param["fit"]["ModelType"]
+    ModelType       = 1#param["fit"]["ModelType"]
+    
+    Nwindows   = param["fit"]["windows"]["number"]
 
     # Load the data file
-    #W, F, E         = np.genfromtxt(dat_directory+param["files"]["datafile"]
-    #                  ,unpack=True,skip_header=7400,skip_footer= 8500)
     W, F, E         = np.genfromtxt(dat_directory+param["files"]["datafile"]
                       ,unpack=True)
 
-    fit_start   = param["fit"]["continuum"]["start"]
-    fit_end     = param["fit"]["continuum"]["stop"]
+    if Nwindows == 2:
+        fit_start_w1   = param["fit"]["windows"]["window1"]["start"]
+        fit_end_w1     = param["fit"]["windows"]["window1"]["stop"]
     
-    s_i = []
-    for i in range(len(W)):
-        if fit_start <= W[i] <= fit_end:
-            s_i.append(i)
-    
-    W    = W[s_i[0]:s_i[-1]]
-    F    = F[s_i[0]:s_i[-1]]
-    E    = E[s_i[0]:s_i[-1]]
+        s1_i = []
+        s2_i = []
+        for i in range(len(W)):
+            if fit_start_w1 <= W[i] <= fit_end_w1:
+                s1_i.append(i)
+            if fit_start_w2 <= W[i] <= fit_end_w2:
+                s2_i.append(i)
+        
+        W1    = W1[s1_i[0]:s1_i[-1]]
+        F1    = F1[s1_i[0]:s1_i[-1]]
+        E1    = E1[s1_i[0]:s1_i[-1]]
+
+        W2    = W2[s2_i[0]:s2_i[-1]]
+        F2    = F2[s2_i[0]:s2_i[-1]]
+        E2    = E2[s2_i[0]:s2_i[-1]]
                       
-    # Create an array of RV measurements with a resolution of 1 km/s
-    v               = np.arange(-len(W)-250,len(W)+250,1) # RV values
+        # Create an array of RV measurements with a resolution of 1 km/s
+        v1    = np.arange(-len(W1)-250,len(W1)+250,1) # RV values
+        v2    = np.arange(-len(W2)-250,len(W2)+250,1) # RV values
     
-    # Calculate the corresponding wavelengths
-    l               = (W[0]+W[-1])/2.*(1.0 + v/3e5)
+        # Calculate the corresponding wavelengths
+        l1    = (W1[0]+W1[-1])/2.*(1.0 + v1/3e5)
+        l2    = (W2[0]+W2[-1])/2.*(1.0 + v2/3e5)
     
     # Select the number of lines to model
-    if param["lines"]["total"] == 3:
-        L1  = param["lines"]["line"]["N1"]["Wavelength"]*(1.0 + v/3e5)
-        L2  = param["lines"]["line"]["N2"]["Wavelength"]*(1.0 + v/3e5)
-        L3  = param["lines"]["line"]["N3"]["Wavelength"]*(1.0 + v/3e5)
+    if param["lines"]["total"] == 5:
+        L1  = param["lines"]["line"]["N1"]["Wavelength"]*(1.0 + v1/3e5)
+        L2  = param["lines"]["line"]["N2"]["Wavelength"]*(1.0 + v1/3e5)
+        L3  = param["lines"]["line"]["N3"]["Wavelength"]*(1.0 + v1/3e5)
+        
+        L4  = param["lines"]["line"]["Nw1"]["Wavelength"]*(1.0 + v2/3e5)
+        L5  = param["lines"]["line"]["Nw2"]["Wavelength"]*(1.0 + v2/3e5)
     
     # Select the model type
     if ModelType == 1:
                 
         # Fixed paramteres
-        Const   =   [W,F,E,l,L1,L2,L3,param["BetaPictoris"]["RV"],
+        Const   =   [W1,W2,F1,F2,E1,E2,l1,l2,L1,L2,L3,L4,L5,param["BetaPictoris"]["RV"],
                     
                     # Fixed ISM parameters
                     param["fit"]["ISM"]["log(H)"],
                     param["fit"]["ISM"]["RV"],
-                    param["fit"]["ISM"]["b"],
                     param["fit"]["ISM"]["T"],
                     
-                    # Fixed disk parameters
+                    # Fixed CS parameters
                     param["fit"]["disk"]["RV"],
-                    param["fit"]["disk"]["b"],
-                    param["fit"]["disk"]["T"]]
+                    param["fit"]["disk"]["T"],
+                    
+                    param["fit"]["exocomet"]["T"]]
 
-                    # Free disk parameters
-        Par     =   [param["fit"]["disk"]["log(H)"]]
+                    # Free ISM parameters
+        Par     =   [param["fit"]["ISM"]["b"],
+                    
+                    # Free CS parameters
+                    param["fit"]["disk"]["log(H)"],
+                    param["fit"]["disk"]["b"],
+                    
+                    # Free exocomet parameters
+                    param["fit"]["exocomet"]["log(H)"],
+                    param["fit"]["exocomet"]["RV"],
+                    param["fit"]["exocomet"]["b"]]
         
         print "\nStarting paramters:"        
         PrintParams(Par)
@@ -123,11 +157,10 @@ def main():
         print "Best fit paramters:"
         PrintParams(P)
         
-        f_fit, f_abs_ism, f_abs_bp   = m.LyModel(P,Const,ModelType,param)
+        f_fit, f_abs_ism, f_abs_bp, f_abs_X   = m.LyModel(P,Const,ModelType,param)
 
-        BasicPlot(param, W, F, E, l, f_fit, f_abs_ism, f_abs_bp)
+        BasicPlot(param, W, F, E, l, f_fit, f_abs_ism, f_abs_bp, f_abs_X)
 
 
 if __name__ == '__main__':
     main()
-
