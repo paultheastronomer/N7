ace75c2d231f197c355532ddcf360a611385bf1b
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-10-17 14:49:34 +0200

    added third window feature

--- MCMC_XA.py
+++ MCMC_XA.py
@@ -46,6 +46,12 @@ def main():
         W2, F2, E2, v2, l2  = c.Window(param,W,F,E,"window2")
         ConstA              = [W1,W2,F1,F2,E1,E2,l1,l2]
 
+    if Nwindows == 3:
+        W1, F1, E1, v1, l1  = c.Window(param,W,F,E,"window1")
+        W2, F2, E2, v2, l2  = c.Window(param,W,F,E,"window2")
+        W3, F3, E3, v3, l3  = c.Window(param,W,F,E,"window3")
+        ConstA              = [W1,W2,W3,F1,F2,F3,E1,E2,E3,l1,l2,l3]
+
     # The parameters listed in ConstB are not dependant on the number of windows used.
     # ConstA is.       
     ConstB   =  [param["BetaPictoris"]["RV"],
@@ -82,7 +88,7 @@ def main():
 
     chain, moves = mc.McMC(W,X,m.Model, ModelType, param, Par, Const, step,1e4)
     
-    outfile = 'chains/chain_Uu_'+sys.argv[1]
+    outfile = 'chains/chain_V3_'+sys.argv[1]
     np.savez(outfile, nh_ISM = chain[:,0], b_ISM = chain[:,1], nh_CS = chain[:,2], b_CS = chain[:,3], nh_X = chain[:,4], RV_X = chain[:,5], b_X = chain[:,6])
 
     Pout = chain[moves,:]
