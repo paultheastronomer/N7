59bb04dcc93f4c4f987a985a1978fbac99a9e4e8
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-11-24 18:06:36 +0100

    simplified submit process

--- condor.py
+++ condor.py
@@ -6,18 +6,11 @@ Written by: Dr. Paul A. Wilson (paul.wilson@iap.fr)
 from condorpy import Job, Templates
 import numpy as np
 
-# A list of cores used
-cores = np.arange(20)+1
+number_of_chains = 100
 
-# A list of nodes used:
-#nodes = ['exa01','exa02','exa03','exa04','exa11','exa12','exa13','exa14','exa21','exa22','exa23','exa24']
-nodes = ['exa14','exa21']
-
-for i in range(len(nodes)):
-	for j in range(len(cores)):
-		print "running core ",j+1," on node ",nodes[i]+".iap.fr"
-		job = Job('MCMC_NI_'+str(nodes[i])+"_"+str(j+1),attributes=Templates.base)
-		job.executable = "MCMC_XA.py"
-		job.arguments = str(j+1)
-		job.requirements = "Machine=="+nodes[i]+".iap.fr"
-		job.submit()
+job = Job("JobName")
+for i in range(number_of_chains):
+	print "Running chain #",i+1
+	job.executable = "MCMC_XA.py"
+	job.arguments = str(i+1)
+	job.submit()
